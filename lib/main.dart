5695905125f4df14f38384c930994cb520439c2b
Basil Ahmed <basilbeso2006@gmail.com>
2023-09-23 23:32:23 +0300

    + localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:islami/ui/HadethDetails/Hadeth_Details_Screen.dart';
 import 'package:islami/ui/chapter_details/chapter_details_screen.dart';
 import 'package:islami/ui/homescreen/HomeScreen.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -53,6 +55,16 @@ class MyApp extends StatelessWidget {
         HadethDetailsScreen.routeName: (buildContext) => HadethDetailsScreen(),
       },
       initialRoute: HomeScreen.routeName,
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en'), // English
+        Locale('ar'), // Spanish
+      ],
     );
   }
 }
